d9a796aba26056a50c2a4b44a54d068fc23eec2f
Rahul Roy <29518806+rahulroy0611@users.noreply.github.com>
2025-06-17 17:23:44 +0530

    Update configure_2.sh

--- configure_2.sh
+++ configure_2.sh
@@ -6,3 +6,17 @@ docker compose up -d
 cd ..
 cd hive_cortex_elasticsearch
 docker compose up -d
+cat << EOF
+-----------------------------------------------------
+MISP URL: https://${PUBLICIP}
+Username: admin@admin.test
+Password: admin
+-----------------------------------------------------
+TheHive URL: http://${PUBLICIP}:9000
+Username: admin
+Password: secret
+-----------------------------------------------------
+Cortex URL: http://${PUBLICIP}:9001
+You need to create credential after opening the url
+-----------------------------------------------------
+EOF
